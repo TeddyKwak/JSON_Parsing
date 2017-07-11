11947e5e0c8454c486fcd5fffa1b0b6509aeca4b
Kwak Teddy <teddy.kwak@ryencatchers.com>
2017-07-11 18:35:20 +0900

    test

--- JSON_Parsing/ViewController.swift
+++ JSON_Parsing/ViewController.swift
@@ -8,11 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct WebsiteDescription: Decodable{
+    let name: String
+    let description: String
+    let courese: [Course]
+}
 
+struct Course: Decodable {
+    let id : Int?
+    let name : String?
+    let link : String?
+    let imageUrl : String?
+    
+//    init(json: [String: Any]) {
+//        id = json["id"] as? Int ?? -1
+//        name = json["name"] as? String ?? ""
+//        link = json["link"] as? String ?? ""
+//        imageUrl = json["imageUrl"] as? String ?? ""
+//    }
+    
+    
+}
+
+class ViewController: UIViewController {
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error?.localizedDescription as Any)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                print(courses)
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+            
+        }.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
